e5bd695613c39fe49824a3ed4cccf12cc99c8dd9
durianice <135111234+durianice@users.noreply.github.com>
2024-09-29 23:36:07 +0800

    Update block-you.js

--- surge/modules/linuxdo/block-you.js
+++ surge/modules/linuxdo/block-you.js
@@ -21,7 +21,7 @@ let result = {};
       .map(i => i.trim())
       .filter(i => i.length)
     $.log('ℹ️ names', names)
-	let enableNotify = ($.lodash_get(arg, 'NOTIFY') || '1')
+	let enableNotify = ($.lodash_get(arg, 'NOTIFY') || 'on')
     // $.log('$response', $.toStr($response))
     let { body } = $response;
     body = JSON.parse(body);
@@ -48,7 +48,7 @@ let result = {};
         return true;
     });
 		
-		if (enableNotify == '1' && res.length) {
+		if (enableNotify === 'on' && res.length) {
 			await notify('Linux.do', '🔇已屏蔽以下用户的话题', `${res.join(',')}`)
     }
     result = {
